373b01fcb478e4d87ed0689ab6c50ec044ee7431
Daniel Medina <paginamedina@gmail.com>
2026-01-18 14:49:14 -0500

    fixing dtos fro strategies

--- src/strategies/dtos/createStrategy.dto.ts
+++ src/strategies/dtos/createStrategy.dto.ts
@@ -1,8 +1,35 @@
-export class CreateStrategyDto {
+import { v4 as uuidv4 } from 'uuid';
+
+export interface CreateStrategyRequest {
+  id?: string;
   strategyName: string;
   entryType: string;
   exitType: string;
   user: string;
   date: string;
   update?: string;
+}
+
+export interface Strategy extends CreateStrategyRequest {
+  id: string;
+}
+
+export class CreateStrategyDto implements Strategy {
+  id: string;
+  strategyName: string;
+  entryType: string;
+  exitType: string;
+  user: string;
+  date: string;
+  update?: string;
+
+  constructor(data: CreateStrategyRequest) {
+    this.id = data.id || uuidv4();
+    this.strategyName = data.strategyName;
+    this.entryType = data.entryType;
+    this.exitType = data.exitType;
+    this.user = data.user;
+    this.date = data.date;
+    this.update = data.update;
+  }
 }
--- src/strategies/dtos/updateStrategy.dto.ts
+++ src/strategies/dtos/updateStrategy.dto.ts
@@ -1,6 +1,20 @@
-export class UpdateStrategyDto {
+export interface UpdateStrategyRequest {
   strategyName?: string;
   entryType?: string;
   exitType?: string;
   update?: string;
+}
+
+export class UpdateStrategyDto {
+  strategyName?: string;
+  entryType?: string;
+  exitType?: string;
+  update: string;
+
+  constructor(data: UpdateStrategyRequest) {
+    if (data.strategyName) this.strategyName = data.strategyName;
+    if (data.entryType) this.entryType = data.entryType;
+    if (data.exitType) this.exitType = data.exitType;
+    this.update = data.update || new Date().toISOString();
+  }
 }
